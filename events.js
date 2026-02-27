ac4a2aafaee74a6d8634658ce9731ed429344ae2
ChandrasekharBali <chandrasekhar2bali@gmail.com>
2026-02-28 00:38:14 +0530

    js events

---
+++ events.js
@@ -0,0 +1,12 @@
+// Task 1 (Click Event)
+// Create on button Click Me. 
+// On the click on button showcase alert with text Hello Javascript
+
+function clickme() {
+    alert('Hello Javascript');
+}
+
+// Task 2 (onblur Event or onchange)
+// Take three textboxes.
+// Do multiplication of textbox 1 and 2 and store the result in textbox 3.
+
